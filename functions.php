3767dfcac554bb3fa228291d4685af3981d5070a
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-05-06 18:27:53 +0200

    update

--- functions.php
+++ functions.php
@@ -21,6 +21,6 @@ require_once get_template_directory() . '/functions/custom-sub-menu-cpt-backoffi
 
 require_once get_template_directory() . '/functions/emails/afficher-entree-mail-back-office.php';
 
-require_once get_template_directory() . '/functions/emails/ajout-page-entree-mail.php';
+// require_once get_template_directory() . '/functions/emails/ajout-page-entree-mail.php';
 
-require_once get_template_directory() . '/functions/emails/traitement-mails.php';
+// require_once get_template_directory() . '/functions/emails/traitement-mails.php';
