fe86fed958323d8fdbea65ce4966c818b87e85d3
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-05-06 20:46:22 +0200

    blocks css

--- functions.php
+++ functions.php
@@ -3,16 +3,16 @@
 
 require_once get_template_directory() . '/functions/enqueue-style.php';
 
-require_once get_template_directory() . '/functions/register-blocks.php';
+require_once get_template_directory() . '/functions/register-blocks.php';/* 
 
 require_once get_template_directory() . '/functions/add-page-options.php';
 
 require_once get_template_directory() . '/functions/role-editor.php';
 
-require_once get_template_directory() . '/functions/custom-class-menu.php';
-/* 
+require_once get_template_directory() . '/functions/custom-class-menu.php'; */
+
 require_once get_template_directory() . '/functions/add-menu-support-and-register-menus.php';
- */
+
 /* 
 require_once get_template_directory() . '/functions/register-main-script.php';
 
