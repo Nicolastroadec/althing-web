339bbf3e7b846988a01c8dda48b68a640cc68b44
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-05-06 20:39:33 +0200

    blocks css

--- functions/register-map-script.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-function registerMapScript()
-{
-    $map_script_timestamp = filemtime(get_template_directory() . '/js/main.js');
-    wp_enqueue_script('mobin-map-script', get_template_directory_uri() . '/js/map.js', array('jquery'), $map_script_timestamp, true);
-}
-
-add_action('wp_enqueue_scripts', 'registerMapScript');
