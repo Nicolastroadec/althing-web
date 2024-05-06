fec317d465a13c310aab286d52b6ad9047e69302
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-05-06 20:44:55 +0200

    blocks css

--- functions.php
+++ functions.php
@@ -13,12 +13,12 @@ require_once get_template_directory() . '/functions/custom-class-menu.php';
 
 require_once get_template_directory() . '/functions/add-menu-support-and-register-menus.php';
 
-
+/* 
 require_once get_template_directory() . '/functions/register-main-script.php';
 
 require_once get_template_directory() . '/functions/custom-sub-menu-cpt-backoffice.php';
 
-require_once get_template_directory() . '/functions/emails/afficher-entree-mail-back-office.php';
+require_once get_template_directory() . '/functions/emails/afficher-entree-mail-back-office.php'; */
 
 // require_once get_template_directory() . '/functions/emails/ajout-page-entree-mail.php';
 
